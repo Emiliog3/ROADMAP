794a1ca61cc33025cffafcc9633fc3ea74f4c287
Emiliog3 <emilio.gomez@est.iudigital.edu.co>
2024-04-20 15:02:49 -0500

    Actualización Archivos

---
+++ python/02_funciones_alcance.py
@@ -0,0 +1,15 @@
+# Funciones y Alcance
+# Una función es un bloque de código que solo se ejecuta cuando se llama y se puede reutilizar en cualquier parte del programa cada vez que se llame.
+
+# Funciones definidas por el usuario
+# En Python, una función se define utilizando la palabra clave def.
+# def nombre_funcion(parametros):
+
+def greet():
+    print("Hello, World!")
+
+# Llamar a la función
+greet()
+
+a = 1 + 2
+print(a)
