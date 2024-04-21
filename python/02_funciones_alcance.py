45db43379fd64ddd0d66b48d6bd209c6fef224fa
Emmmiliog3 <emilio.gomez@est.iudigital.edu.co>
2024-04-21 14:21:06 -0500

    Actualización: cambios realizados en el archivo X

--- python/02_funciones_alcance.py
+++ python/02_funciones_alcance.py
@@ -11,5 +11,65 @@ def greet():
 # Llamar a la función
 greet()
 
-a = 1 + 2
-print(a)
+# Con retonro
+
+def retonr_greet():
+    return "Hello, Pablo!" 
+
+print(retonr_greet())
+
+# Con un argumento
+
+def greet_name(name):
+    return "Hello, " + name
+
+print(greet_name("Pablo Emilio"))
+
+def arg_greet(name):
+    print(f"Hello, {name}") 
+arg_greet("Pablo Emilio Gómez")
+
+
+def arg_greet(name, age):
+    print(f"Hello, {name} you are {age} years old")
+
+arg_greet("Pablo Emilio Gómez", 25)
+
+
+def default_greet(name = "Pablo Emilio Gómez"):
+    print(f"Hello, {name}")
+
+default_greet("Python")
+default_greet()
+
+
+# Con arguentos
+
+def return_args_greet(greet, name):
+    return f"{greet}, {name} !"
+
+print(return_args_greet("Hello", "Pablo Emilio Gómez"))
+
+
+
+def return_args_greet(greet, name):
+    return f"{greet}, {name}!"
+
+print(return_args_greet("Hello", "Pablo Emilio Gómez"))
+
+# Con retonro de varios valores
+
+def multiple_return_greet():
+    return "Hello", "Pablo Emilio Gómez Gómez"
+
+greet, name = multiple_return_greet()
+print(greet)
+print(name)
+
+# Con un número variable de argumentos
+
+def variable_arg_greet(*names):
+    for name in names:
+        print(f"Hello, {name}")
+
+variable_arg_greet("Pablo", "Emilio", "Gómez", "Comunidad")      
